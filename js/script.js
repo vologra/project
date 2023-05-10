f7a7a30bc4dc98d555612ae00999d1b48a15f66c
vologra <132639867+vologra@users.noreply.github.com>
2023-05-10 20:34:23 +0900

    Update script.js
    modifaka

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-'use strict'
+'use strict';
 
-// alert ('halo')
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const result = confirm ('do you wanna peace of me?');
-// console.log(result);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const answer = prompt('why are you gay?', 'who says im gay'); 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-const answers = [];
-answers[0] = prompt('name?');
-answers[1] = prompt('surname?');
-answers[2] = prompt('age?');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(typeof(answers));
+console.log(personalMovieDB);
